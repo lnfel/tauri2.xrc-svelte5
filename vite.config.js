f5fe380bbfe2212dd1fc1cf7fd20841a072d9afc
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2024-08-30 08:47:13 +0800

    [Change] Follow tauri setup for sveltekit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,36 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { sveltekit } from '@sveltejs/kit/vite'
+import { defineConfig } from 'vitest/config'
+
+const host = process.env.TAURI_DEV_HOST
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
-});
+    plugins: [sveltekit()],
+    /**
+     * Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+     * 
+     * 1. prevent vite from obscuring rust errors
+     */
+    clearScreen: false,
+    /**
+     * 2. tauri expects a fixed port, fail if that port is not available
+     */
+    server: {
+        port: 5173,
+        strictPort: true,
+        host: host || false,
+        hmr: host
+            ? {
+                protocol: "ws",
+                host,
+                port: 5173
+            }
+            : undefined,
+        watch: {
+            // 3. tell vite to ignore watching `src-tauri`
+            ignored: ["**/src-tauri/**"],
+        }
+    },
+    test: {
+        include: ['src/**/*.{test,spec}.{js,ts}']
+    }
+})
